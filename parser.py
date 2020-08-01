085e8355e83dbd25e12c0fb3218e2451ee13ab96
Nikolay <nakozlov2018@yandex.ru>
2020-08-01 19:53:06 +0300

    finished main functionality

--- parser.py
+++ parser.py
@@ -1,36 +1,67 @@
 from bs4 import BeautifulSoup
 import requests
-import html
-link = 'https://hh.ru/search/vacancy?area=1&st=searchVacancy&fromSearch=true&items_on_page=100'
+import time
+import csv
+
+link = 'https://hh.ru/search/vacancy?area=1&st=searchVacancy&items_on_page=100&search_period=1'
+
+
+def save(vacancies, path):
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(("Вакансия", "Компания", "Ссылка", "Зарплата"))
+
+        for vacancy in vacancies:
+            try:
+                writer.writerow((
+                    vacancy['title'],
+                    vacancy['company'],
+                    vacancy['link'],
+                    vacancy['salary']
+                ))
+            except UnicodeEncodeError:
+                continue
+
+
 def get_html(url):
     headers = {'User-Agent': 'PostmanRuntime/7.26.1'}
-    r = requests.get(link,headers=headers).text
+    r = requests.get(url, headers=headers).text
     return BeautifulSoup(r)
 
 
-html = get_html(link)
-#number of pages
-last_page = html.find_all('a', attrs={'data-qa':'pager-page'})[-1].text
-
-#metro branches from 1 to 12
-for i in range(1,13):
-    print(i)
-# def get_data():
-#     vacancies = []
-#     for el in soup.find_all(attrs={"class": "vacancy-serp-item"}):
-#         try:
-#             salary = el.find('span', attrs={'data-qa':'vacancy-serp__vacancy-compensation'}).text.replace('\xa0','')
-#         except:
-#             salary = ''
-
-#         vacancies.append({
-#             "title": el.find('span', class_='g-user-content').text,
-#             "company": el.find('span', class_='g-user-content').text,
-#             "link": el.find('a', attrs={'data-qa':'vacancy-serp__vacancy-employer'}).text,
-#             "salary": salary
-#         })
+def get_vacancies(soup):
+    vacancies = []
+    for el in soup.find_all(attrs={"class": "vacancy-serp-item"}):
+        # try to parse salary
+        try:
+            salary = el.find('span', attrs={
+                             'data-qa': 'vacancy-serp__vacancy-compensation'}).text.replace('\xa0', '')
+        except:
+            salary = ''
+        vacancies.append({
+            "title": el.find('span', class_='g-user-content').text,
+            "company": el.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text,
+            "link": el.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href'],
+            "salary": salary
+        })
+    return vacancies
 
 
+# metro lines from 1 to 12 in search parameters
+vacancies = []
+for line in range(1, 13):
+    soup = get_html(f'{link}&metro={line}')
+    # Finds number of pages or sets the number as one
+    try:
+        number_of_pages = int(soup.find_all(
+            'a', attrs={'data-qa': 'pager-page'})[-1].text)
+    except IndexError:
+        number_of_pages = 1
+    for page in range(number_of_pages):
+        soup = get_html(f'{link}&metro={line}&page={page}')
+        time.sleep(1)
+        data = get_vacancies(soup)
+        vacancies.extend(data)
 
-# for vacancy in vacancies:
-#     print(vacancy)
+
+save(vacancies, 'vacs.csv')
